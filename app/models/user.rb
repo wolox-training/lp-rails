82a3214887fa9789711b5b248289b3fc0c384348
Lucas Polonuer <polonuer@gmail.com>
2017-08-04 16:18:33 -0300

    Fixed code offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 class User < ApplicationRecord
-
   validates :first_name, :last_name, :email, :password, presence: true
   validates :email, uniqueness: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable
-
-
+         :recoverable, :rememberable, :trackable, :validatable
 end
