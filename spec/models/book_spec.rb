1863c38da8891446d86596040d2ed3df799ad870
Lucas Polonuer <polonuer@gmail.com>
2017-08-09 14:24:13 -0300

    CHanges requested

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,94 +1,18 @@
 require 'rails_helper'
 
 describe Book, type: :model do
-  it do
-    should validate_presence_of(:genre)
-  end
-  it do
-    should validate_presence_of(:author)
-  end
-  it do
-    should validate_presence_of(:image)
-  end
-  it do
-    should validate_presence_of(:title)
-  end
-  it do
-    should validate_presence_of(:publisher)
-  end
-  it do
-    should validate_presence_of(:year)
-  end
+  it { validate_presence_of(:genre) }
+  it { validate_presence_of(:author) }
+  it { validate_presence_of(:image) }
+  it { validate_presence_of(:title) }
+  it { validate_presence_of(:publisher) }
+  it { validate_presence_of(:year) }
 
   subject(:book) do
-    described_class.new(
-      genre: genre,
-      author: author,
-      image: image,
-      title: title,
-      publisher: publisher,
-      year: year
-    )
+    create(:book)
   end
 
-  let(:genre)     { Faker::Book.genre }
-  let(:author)    { Faker::Book.author }
-  let(:image)     { Faker::Internet.url('image-host.com', '/image.png') }
-  let(:title)     { Faker::Book.title }
-  let(:publisher) { Faker::Book.publisher }
-  let(:year)      { Faker::Number.between(1800, 2017) }
-
-  it 'is valid with valid parameters' do
+  it 'has a valid factory' do
     is_expected.to be_valid
-  end
-
-  describe '#create' do
-    context 'When the genre is nil' do
-      let(:genre) { nil }
-
-      it do
-        is_expected.not_to be_valid
-      end
-    end
-
-    context 'When the author is nil' do
-      let(:author) { nil }
-
-      it do
-        is_expected.not_to be_valid
-      end
-    end
-
-    context 'When the image is nil' do
-      let(:image) { nil }
-
-      it do
-        is_expected.not_to be_valid
-      end
-    end
-
-    context 'When the title is nil' do
-      let(:title) { nil }
-
-      it do
-        is_expected.not_to be_valid
-      end
-    end
-
-    context 'When the publisher is nil' do
-      let(:publisher) { nil }
-
-      it do
-        is_expected.not_to be_valid
-      end
-    end
-
-    context 'When the year is nil' do
-      let(:year) { nil }
-
-      it do
-        is_expected.not_to be_valid
-      end
-    end
   end
 end
