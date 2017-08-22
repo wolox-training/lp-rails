b1d8353775f35232c198d243ac00519dc5da884c
Lucas Polonuer <polonuer@gmail.com>
2017-08-22 15:08:17 -0300

    Created BookSuggestion Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170818181516) do
+ActiveRecord::Schema.define(version: 20170822174844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,20 @@ ActiveRecord::Schema.define(version: 20170818181516) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "book_suggestions", force: :cascade do |t|
+    t.string "editorial"
+    t.float "price"
+    t.string "author", null: false
+    t.string "title", null: false
+    t.string "link", null: false
+    t.string "publisher"
+    t.string "year"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_book_suggestions_on_user_id"
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "genre", default: "", null: false
     t.string "author", default: "", null: false
@@ -76,6 +90,7 @@ ActiveRecord::Schema.define(version: 20170818181516) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "book_suggestions", "users"
   add_foreign_key "rents", "books"
   add_foreign_key "rents", "users"
 end
