c109c1b4bf2c3d1ea72a62a1428d841e24cdf5a6
Lucas Polonuer <polonuer@gmail.com>
2017-08-25 16:20:19 -0300

    Solved requested changed

--- app/controllers/api/v1/book_suggestions_controller.rb
+++ app/controllers/api/v1/book_suggestions_controller.rb
@@ -3,26 +3,34 @@ module Api
     class BookSuggestionsController < ApplicationController
       skip_before_action :verify_authenticity_token
 
-      # rubocop:disable Metrics/AbcSize
       def create
-        book_suggestion = BookSuggestion.new(editorial: params[:editorial],
-                                                 price: params[:price],
-                                                 author: params[:author],
-                                                 title: params[:title],
-                                                 link: params[:link],
-                                                 publisher: params[:publisher],
-                                                 year: params[:year],
-                                                 user: user)
-        render_error('Invalid parameters', :unprocessable_entity) and return unless book_suggestion.valid?
+        book_suggestion = BookSuggestion.new(book_suggestions_params)
+        error_invalid_params && return unless book_suggestion.valid?
         book_suggestion.save
         render json: book_suggestion, status: :created
       end
-      # rubocop:enable Metrics/AbcSize
+
+      def new
+        @book_suggestion = BookSuggestion.new
+      end
 
       private
 
-      def user
-        User.find_by_id(params[:user_id])
+      def book_suggestions_params
+        params.fetch(:book_suggestion, {}).permit(
+          :editorial,
+          :price,
+          :author,
+          :title,
+          :link,
+          :publisher,
+          :year,
+          :user_id
+        )
+      end
+
+      def error_invalid_params
+        render_error('Invalid parameters', :unprocessable_entity)
       end
 
       def render_error(error_message, status)
